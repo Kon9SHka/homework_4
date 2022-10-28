67e78c94a4f467e106fd653b16a574aab7ed7837
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2022-10-28 14:47:07 +0300

    added homework 4 task 1

---
+++ pr_1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Clear();
+
+int first_number = GetNumberFromUser("Введите первое число: ");
+int second_number = GetNumberFromUser("Введите второе число: ");
+Console.WriteLine($"{DegreeResolution(first_number, second_number)}");
+
+
+int GetNumberFromUser (string message) 
+{
+    while(true) 
+    {
+
+    Console.Write(message);
+    bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+    while(isCorrect)
+        return number;
+    Console.WriteLine("Ошибка ввода!");
+
+    }
+}
+
+int DegreeResolution (int fp, int degree) 
+{
+    int result = fp;
+    for (int i = 1; i < degree; i++) 
+    {
+        result = result * fp;
+    }
+    return result;
+}
