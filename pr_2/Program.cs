aefeccf68ef5b6274affcf894a8febe419ad57de
Кирилл Калинин <kirillkalinin@MacBook-Pro-Kirill.local>
2022-10-28 15:03:07 +0300

    added homework 4 task 2

---
+++ pr_2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+int first_number = GetNumberFromUser("Введите число: ");
+Console.WriteLine($"{SumResolution(first_number)}");
+
+
+int GetNumberFromUser (string message) 
+{
+    while(true) 
+    {
+
+    Console.Write(message);
+    bool isCorrect = int.TryParse(Console.ReadLine(), out int number);
+    while(isCorrect)
+        return number;
+    Console.WriteLine("Ошибка ввода!");
+
+    }
+}
+
+int SumResolution (int number) 
+{
+    int result = 0;
+    int current_number = number;
+    while(current_number > 0) 
+    {
+        result = result + (current_number%10);
+        current_number = current_number/10;
+    }
+    return result;
+}
